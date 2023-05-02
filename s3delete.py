3c24dad81efc8ab6d5c1ba8b5816e2e25551cdc3
Stepn_V <stepan.vladovskiy@gmail.com>
2023-05-02 16:31:45 +0000

    feat: add pagination

--- s3delete.py
+++ s3delete.py
@@ -1,48 +1,40 @@
 import os
 import boto3
-
 from dotenv import load_dotenv
 
-
-
-# Load the environment variables from the .env file
 load_dotenv()
 
-# Now you can access the environment variables using os.getenv
+# Establish the S3 client
 s3 = boto3.client('s3',
-                  aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
-                  aws_secret_access_key=os.getenv('S3_SECRET_KEY'),
+                  aws_access_key_id=os.environ['S3_ACCESS_KEY'],
+                  aws_secret_access_key=os.environ['S3_SECRET_KEY'],
                  )
 
-bucket = os.getenv('BUCKET')
+bucket = os.environ['BUCKET']
 
-# Function to delete a file from a bucket
 def delete_file_from_bucket(bucket_name, file_name, s3_client):
-    s3_client.delete_object(
-        Bucket=bucket_name,
-        Key=file_name
-    )
+    """Delete a file from the S3 bucket."""
+    s3_client.delete_object(Bucket=bucket_name, Key=file_name)
 
-# Function to find a file in a bucket
 def find_file_in_bucket(bucket_name, file_name, s3_client):
-    response = s3_client.list_objects_v2(
-        Bucket=bucket_name,
-    )
-
-    for item in response['Contents']:
-        if file_name in item['Key']:
-            return item['Key']
+    """Find a file in the S3 bucket."""
+    paginator = s3_client.get_paginator('list_objects_v2')
+    for page in paginator.paginate(Bucket=bucket_name):
+        if 'Contents' in page:
+            for item in page['Contents']:
+                if file_name in item['Key']:
+                    return item['Key']
     return None
 
 # Read the file names from the file
-with open('files_to_delete.txt', 'r') as file:
+with open('files_to_delete.txt', 'r', encoding='utf-8') as file:
     files_to_delete = [line.strip() for line in file]
 
 # Iterate over the list of files and delete each one if it is found in the bucket
-for file_name in files_to_delete:
-    found_file_name = find_file_in_bucket(bucket, file_name, s3)
+for file_names in files_to_delete:
+    found_file_name = find_file_in_bucket(bucket, file_names, s3)
     if found_file_name is not None:
         delete_file_from_bucket(bucket, found_file_name, s3)
         print(f'Deleted {found_file_name} from {bucket}')
     else:
-        print(f'File {file_name} not found in {bucket}')
+        print(f'File {file_names} not found in {bucket}')
